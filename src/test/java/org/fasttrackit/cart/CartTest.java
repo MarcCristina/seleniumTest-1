ddedf69bf0a26ce20f7a5a62615bbc657877af4f
MarcCristina <crys_20062006@yahoo.com>
2018-02-01 21:28:27 +0200

    working in progress

--- src/test/java/org/fasttrackit/cart/CartTest.java
+++ src/test/java/org/fasttrackit/cart/CartTest.java
@@ -1,10 +1,17 @@
 package org.fasttrackit.cart;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CartTest {
 
@@ -21,26 +28,14 @@ public class CartTest {
 
         driver.findElement(By.id("search")).sendKeys("vase" + Keys.ENTER);
 
-        driver.findElement(By.xpath("//div[@class='product-info' and ./descendant::*[text()" +
-                " = 'Herald Glass Vase']]//button[contains(@class, 'btn-cart')]")).click();
+        driver.findElement(By.xpath("//div[@class='product-info' and ./descendant::*[text()" + " = 'Herald Glass Vase']]//button[contains(@class, 'btn-cart')]")).click();
 
+        WebElement successMessageContainer = driver.findElement(By.cssSelector (".success-msg"));
+        assertThat("Product not added to cart.", successMessageContainer.getText(), containsString("Herald Glass Vase"));
 
+// todo: assert that product is present in cart
     }
 
-    @Test
-    public void specialPriceDisplayAfterSimpleSearch() {
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\Selenium driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
 
-        driver.get("https://fasttrackit.org/selenium-test/");
 
-        driver.findElement(By.id("search")).sendKeys("vase" + Keys.ENTER);
-
-        driver.findElement(By.xpath("//p[@class='old-price']//span[@class='price']")).getText();
-
-        String oldPrice = driver.findElement(By.xpath("//p[@class='old-price']//span[@class='price']")).getText();
-        String specialPrice = driver.findElement(By.xpath("//p[@class='special-price']//span[@class='price']")).getText();
-
-    }
 }
--- src/test/java/org/fasttrackit/search/SimpleSearchTest.java
+++ src/test/java/org/fasttrackit/search/SimpleSearchTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SimpleSearchTest {
 
@@ -20,7 +26,36 @@ public class SimpleSearchTest {
 
         driver.findElement(By.id("search")).sendKeys("vase" + Keys.ENTER);
 
+    }
 
+    @Test
+    public void specialPriceDisplayAfterSimpleSearch() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Selenium driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://fasttrackit.org/selenium-test/");
+
+        String keyword = "vase";
+        driver.findElement(By.id("search")).sendKeys(keyword + Keys.ENTER);
+
+        System.out.println("Pressed Enter in search field.");
+
+        driver.findElement(By.xpath("//p[@class='old-price']//span[@class='price']")).getText();
+
+        String oldPrice = driver.findElement(By.xpath("//p[@class='old-price']//span[@class='price']")).getText();
+        String specialPrice = driver.findElement(By.xpath("//p[@class='special-price']//span[@class='price']")).getText();
+
+        List<WebElement> productNames = driver.findElements(By.cssSelector(".product-name a"));
+
+        System.out.println("Stored" + productNames.size() + "product names from search results");
+
+        for (WebElement productName : productNames) {
+
+            assertThat("Some of the product names do not contain the searched keyword.",
+                    productName.getText(), containsString(keyword.toUpperCase()));
+
+        }
 
     }
 }
